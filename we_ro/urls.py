cfe2dbe94ae598862ed8815d12a3e1c2be644f67
kst6294 <kst6294@gmail.com>
2020-12-30 19:26:22 +0900

    Add: Signup 기능 구현 - email, password, date_of_birth, phonenumber 정규식에 의한 validation   기능 구현 - password 암호화 기능 구현 - 유닛테스트를 위한 tests.py 작성

--- we_ro/urls.py
+++ we_ro/urls.py
@@ -16,7 +16,5 @@ Including another URLconf
 from django.urls import path, include
 
 urlpatterns = [
-#    path('user', include('user.urls')),
-#    path('music', include('content.urls')),
-#    path('storage', include('storage.urls')),
+    path('users', include('user.urls')),
 ]
