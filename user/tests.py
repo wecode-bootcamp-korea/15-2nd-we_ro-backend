19db053d5d6bc1bc06649b1ed6a3fd5062169657
kst6294 <kst6294@gmail.com>
2020-12-30 20:15:34 +0900

    Add: Signin 기능 구현 - signin 기능 구현 - uservalidation 을 통한 로그인 정보 확인 - kakao 로그인 시 kakao_id 를 받을 수 있는 column 생성 - 일부 테이블 null 허용 - test 코드 작성 - rds 연결

--- user/tests.py
+++ user/tests.py
@@ -1,23 +1,24 @@
 import json
 import bcrypt
+import jwt
 
-from django.test import Client
-from django.test import TestCase
+from django.test      import Client
+from django.test      import TestCase
 
-from user.models import User, Platform
+from user.models import User
+from my_settings import SECRET_KEY, ALGORITHM
 
-
-class UserTest(TestCase):
+class UserSignUpTest(TestCase):
     def setUp(self):
-        Platform.objects.create(name='kakao')
-        Platform.objects.create(name='flo')
-        User.objects.create(
+        password = '123qwe!@#QWE'
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+        User(
             email = 'kst6294@gmail.com',
-            password = '123qwe!@#QWE',
+            password = hashed_password,
             date_of_birth = '1991-09-04',
             phone_number = '010-1234-1234',
-            platform_id = Platform.objects.get(name='kakao').id
-        )
+        ).save()
 
     def tearDown(self):
         User.objects.all().delete()
@@ -29,20 +30,8 @@ class UserTest(TestCase):
             'password' : '123qwe!@#QWE',
             'date_of_birth' : '1991-09-04',
             'phone_number' : '010-1234-1234',
-            'platform' : 'flo'
         }
-        response = client.post('/users/signup', json.dumps(user), content_type='application/json')
-        self.assertEqual(response.status_code, 200)
 
-    def test_signupview_post_create_success_with_duplicated_user_no_duplicated_platform(self):
-        client = Client()
-        user = {
-            'email' : 'kst6294@gmail.com',
-            'password' : '123qwe!@#QWE',
-            'date_of_birth' : '1991-09-04',
-            'phone_number' : '010-1234-1234',
-            'platform' : 'flo'
-        }
         response = client.post('/users/signup', json.dumps(user), content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
@@ -53,8 +42,67 @@ class UserTest(TestCase):
             'password' : '123qwe!@#QWE',
             'date_of_birth' : '1991-09-04',
             'phone_number' : '010-1234-1234',
-            'platform' : 'kakao'
         }
+
         response = client.post('/users/signup', json.dumps(user), content_type='application/json')
         self.assertEqual(response.status_code, 400)
+
+
+class UserSignInTest(TestCase):
+    def setUp(self):
+        password = '123qwe!@#QWE'
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+        User(
+            email = 'kst6294@gmail.com',
+            password      = hashed_password,
+            date_of_birth = '1991-09-04',
+            phone_number  = '010-1234-1234',
+        ).save()
+
+        email         = 'kst6294@gmail.com'
+        password      = '123qwe!@#QWE'
+        date_of_birth = '1991-09-04'
+        phone_number  = '010-1234-1234'
+
+    def tearDown(self):
+        User.objects.all().delete()
+
+    def test_signinview_post_success_with_user(self):
+        client = Client()
+        user = {
+            'email'         : 'kst6294@gmail.com',
+            'password'      : '123qwe!@#QWE',
+            'date_of_birth' : '1991-09-04',
+            'phone_number'  : '010-1234-1234',
+        }
+        response     = client.post('/users/signin', json.dumps(user), content_type='application/json')
+        access_token = response.json()['ACCESS_TOKEN']
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'MESSAGE' : 'SUCCESS', 'ACCESS_TOKEN' : access_token})
+
+    def test_signinview_post_fail_with_no_user(self):
+        client = Client()
+        user = {
+            'email'         : 'suntae@gmail.com',
+            'password'      : '123qwe!@#QWE',
+            'date_of_birth' : '1991-09-04',
+            'phone_number'  : '010-1234-1234',
+        }
+        response     = client.post('/users/signin', json.dumps(user), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'MESSAGE' : 'INVALID_EMAIL_OR_PASSWORD'})
+
+    def test_signinview_post_fail_INVALID_PASSWORD(self):
+        client = Client()
+        user = {
+            'email'         : 'kst6294@gmail.com',
+            'password'      : '123qwe!@#QE',
+            'date_of_birth' : '1991-09-04',
+            'phone_number'  : '010-1234-1234',
+        }
+
+        response        = client.post('/users/signin', json.dumps(user), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'MESSAGE' : 'INVALID_EMAIL_OR_PASSWORD'})
 
