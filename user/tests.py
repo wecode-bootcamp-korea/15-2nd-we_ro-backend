4b791fdd3b8f5b812e5d25e05e29ac3cd9fba798
kst6294 <kst6294@gmail.com>
2021-01-01 16:43:01 +0900

    Add: Password & Phone_number 변경 기능 구현 - current_password 확인 후 new_password 변경 - current_password 확인 후 new_phone_number 변경 - signin decorator - test 코드 작성

--- user/tests.py
+++ user/tests.py
@@ -2,22 +2,22 @@ import json
 import bcrypt
 import jwt
 
-from django.test      import Client
-from django.test      import TestCase
+from django.test   import Client, TestCase
 
-from user.models import User
-from my_settings import SECRET_KEY, ALGORITHM
+from user.models   import User
+from my_settings   import SECRET_KEY, ALGORITHM
+from unittest.mock import patch, MagicMock
 
 class UserSignUpTest(TestCase):
     def setUp(self):
-        password = '123qwe!@#QWE'
+        password        = '123qwe!@#QWE'
         hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
         User(
-            email = 'kst6294@gmail.com',
-            password = hashed_password,
+            email         = 'kst6294@gmail.com',
+            password      = hashed_password,
             date_of_birth = '1991-09-04',
-            phone_number = '010-1234-1234',
+            phone_number  = '010-1234-1234',
         ).save()
 
     def tearDown(self):
@@ -26,10 +26,10 @@ class UserSignUpTest(TestCase):
     def test_signupview_post_create_success_with_no_duplicated_user(self):
         client = Client()
         user = {
-            'email' : 'suntae@gmail.com',
-            'password' : '123qwe!@#QWE',
+            'email'         : 'suntae@gmail.com',
+            'password'      : '123qwe!@#QWE',
             'date_of_birth' : '1991-09-04',
-            'phone_number' : '010-1234-1234',
+            'phone_number'  : '010-1234-1234',
         }
 
         response = client.post('/users/signup', json.dumps(user), content_type='application/json')
@@ -38,10 +38,10 @@ class UserSignUpTest(TestCase):
     def test_signupview_post_create_fail_with_duplicated_user(self):
         client = Client()
         user = {
-            'email' : 'kst6294@gmail.com',
-            'password' : '123qwe!@#QWE',
+            'email'         : 'kst6294@gmail.com',
+            'password'      : '123qwe!@#QWE',
             'date_of_birth' : '1991-09-04',
-            'phone_number' : '010-1234-1234',
+            'phone_number'  : '010-1234-1234',
         }
 
         response = client.post('/users/signup', json.dumps(user), content_type='application/json')
@@ -50,20 +50,15 @@ class UserSignUpTest(TestCase):
 
 class UserSignInTest(TestCase):
     def setUp(self):
-        password = '123qwe!@#QWE'
+        password        = '123qwe!@#QWE'
         hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
         User(
-            email = 'kst6294@gmail.com',
+            email         = 'kst6294@gmail.com',
             password      = hashed_password,
             date_of_birth = '1991-09-04',
             phone_number  = '010-1234-1234',
         ).save()
-
-        email         = 'kst6294@gmail.com'
-        password      = '123qwe!@#QWE'
-        date_of_birth = '1991-09-04'
-        phone_number  = '010-1234-1234'
 
     def tearDown(self):
         User.objects.all().delete()
@@ -76,6 +71,7 @@ class UserSignInTest(TestCase):
             'date_of_birth' : '1991-09-04',
             'phone_number'  : '010-1234-1234',
         }
+
         response     = client.post('/users/signin', json.dumps(user), content_type='application/json')
         access_token = response.json()['ACCESS_TOKEN']
         self.assertEqual(response.status_code, 200)
@@ -89,7 +85,8 @@ class UserSignInTest(TestCase):
             'date_of_birth' : '1991-09-04',
             'phone_number'  : '010-1234-1234',
         }
-        response     = client.post('/users/signin', json.dumps(user), content_type='application/json')
+
+        response = client.post('/users/signin', json.dumps(user), content_type='application/json')
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(), {'MESSAGE' : 'INVALID_EMAIL_OR_PASSWORD'})
 
@@ -102,7 +99,125 @@ class UserSignInTest(TestCase):
             'phone_number'  : '010-1234-1234',
         }
 
-        response        = client.post('/users/signin', json.dumps(user), content_type='application/json')
+        response = client.post('/users/signin', json.dumps(user), content_type='application/json')
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(), {'MESSAGE' : 'INVALID_EMAIL_OR_PASSWORD'})
 
+class UserPasswordCheckTest(TestCase):
+    def setUp(self):
+        password        = '123qwe!@#QWE'
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+        user = User(
+            email         = 'kst6294@gmail.com',
+            password      = hashed_password,
+            date_of_birth = '1991-09-04',
+            phone_number  = '010-1234-1234',
+        )
+
+        user.save()
+
+        self.access_token = jwt.encode({'id' : User.objects.get(id=user.id).id}, SECRET_KEY, algorithm=ALGORITHM)
+        self.user_id      = user.id
+
+    def tearDown(self):
+        User.objects.all().delete()
+
+    def test_password_is_same_check_post_success(self):
+        client = Client()
+
+        headers = {'HTTP_Authorization' : self.access_token}
+
+        user = {
+            'user_id' : self.user_id,
+            'password' : '123qwe!@#QWE',
+        }
+
+        response = client.post('/users/password/check', json.dumps(user), content_type='application/json', **headers)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'MESSAGE' : 'SUCCESS'})
+
+    def test_password_is_same_check_post_fail(self):
+        client = Client()
+
+        headers = {'HTTP_Authorization' : self.access_token}
+
+        user = {
+            'user_id' : self.user_id,
+            'password' : '23eewre!@#QWE',
+        }
+
+        response = client.post('/users/password/check', json.dumps(user), content_type='application/json', **headers)
+
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'MESSAGE' : 'PASSWORD_IS_DIFFERENT'})
+
+class UserPasswordChangeTest(TestCase):
+    def setUp(self):
+        password        = '123qwe!@#QWE'
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+        user = User(
+            email         = 'kst6294@gmail.com',
+            password      = hashed_password,
+            date_of_birth = '1991-09-04',
+            phone_number  = '010-1234-1234',
+        )
+
+        user.save()
+
+        self.access_token = jwt.encode({'id' : User.objects.get(id=user.id).id}, SECRET_KEY, algorithm=ALGORITHM)
+        self.user_id      = user.id
+
+    def tearDown(self):
+        User.objects.all().delete()
+
+    def test_password_change_old_password_to_new_passowrd_post_success(self):
+        client = Client()
+
+        headers = {'HTTP_Authorization' : self.access_token}
+
+        user = {
+            'user_id'      : self.user_id,
+            'new_password' : '2sdf#223QWE!@#',
+        }
+
+        response = client.patch('/users/password/change', json.dumps(user), content_type='application/json', **headers)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'MESSAGE' : 'SUCCESS'})
+
+
+class UserPhoneNumberChangeTest(TestCase):
+    def setUp(self):
+        password        = '123qwe!@#QWE'
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+        user = User(
+            email         = 'kst6294@gmail.com',
+            password      = hashed_password,
+            date_of_birth = '1991-09-04',
+            phone_number  = '010-1234-1234',
+        )
+
+        user.save()
+
+        self.access_token = jwt.encode({'id' : User.objects.get(id=user.id).id}, SECRET_KEY, algorithm=ALGORITHM)
+        self.user_id      = user.id
+
+    def tearDown(self):
+        User.objects.all().delete()
+
+    def test_phone_number_change_old_phone_number_to_new_phone_number_success(self):
+        client = Client()
+
+        headers = {'HTTP_Authorization' : self.access_token}
+
+        user = {
+            'user_id' : self.user_id,
+            'new_phone_number' : '010-4321-4321',
+        }
+
+        response = client.patch('/users/phonenumber/change', json.dumps(user), content_type='application/json', **headers)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'MESSAGE' : 'SUCCESS'})
