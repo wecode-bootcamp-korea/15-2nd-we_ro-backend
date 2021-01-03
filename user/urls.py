90688cc9596fe4ff66b8fa2393484464813fb3e8
kst6294 <kst6294@gmail.com>
2021-01-03 15:20:25 +0900

    Add: kakao api 로그인 기능 구현 - kakao 로그인 - 유저 데이터 저장기능 구현

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from user.views  import SignUpView, SignInView
-
+from user.views  import SignUpView, SignInView, KakaoSignInView, KakaoSignInCallbackView
 
 urlpatterns = [
     path('/signup', SignUpView.as_view()),
     path('/signin', SignInView.as_view()),
+    path('/signin/kakao', KakaoSignInView.as_view()),
+    path('/signin/kakao/callback', KakaoSignInCallbackView.as_view()),
 ]
 
