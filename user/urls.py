31ac9c8a8f90526a1f08648ad610217823493efb
kst6294 <kst6294@gmail.com>
2021-01-06 14:46:56 +0900

    Add: Taste 취향관리 기능 구현 - 캐릭터를 기반으로 하는 취향관리 구현 - Post 방식 -> 취향 생성 - Get 방식 -> 취향 리스트 호출 - character_taste_genre - character_taste_chart - character_taste_artist - 테스트코드 작성

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from user.views  import SignUpView, SignInView, CharacterView
+
+from user.views  import SignUpView, SignInView, TasteArtistView, TasteChartView, TasteGenreView
 
 urlpatterns = [
     path('/signup', SignUpView.as_view()),
     path('/signin', SignInView.as_view()),
-    path('/character', CharacterView.as_view()),
-    path('/character/<int:character_id>', CharacterView.as_view()),
+    path('/taste/artist', TasteArtistView.as_view()),
+    path('/taste/chart', TasteChartView.as_view()),
+    path('/taste/genre', TasteGenreView.as_view()),
 ]
 
