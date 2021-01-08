dcd22b3bd41555fe01f1c72d3d42904f5504898e
kst6294 <kst6294@gmail.com>
2021-01-08 14:35:25 +0900

    사라진 데이터 복구중

--- user/views.py
+++ user/views.py
@@ -44,3 +44,72 @@ class KakaoSignUpView(View):
         except KeyError:
                 return JsonResponse({'MESSAGE' : 'INVALID_KEY'}, status=401)
 
+
+
+class SignUpView(View):
+    def post(self, request):
+        try:
+            data          = json.loads(request.body)
+            email         = data['email']
+            password      = data['password']
+            date_of_birth = data['date_of_birth']
+            phone_number  = data['phone_number']
+
+            user_validation = User.objects.filter(kakao_id__isnull=True).filter(email=email)
+
+            if user_validation:
+                return JsonResponse({'MESSAGE' : 'ACCOUNT_EXISTS_ALREADY'}, status=400)
+
+            if not re.match(REGEX_EMAIL, email) or not re.match(REGEX_PASSWORD, password):
+                return JsonResponse({'MESSAGE' : 'INVALID_EMAIL_OR_PASSWORD'}, status=400)
+
+            if not re.match(REGEX_DATE_OF_BIRTH, date_of_birth):
+                return JsonResponse({'MESSAGE' : 'INVALID_DATE_OF_BIRTH'}, status=400)
+
+            if not re.match(REGEX_PHONE_NUMBER, phone_number):
+                return JsonResponse({'MESSAGE' : 'INVALID_PHONE_NUMBER'}, status=400)
+
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+            User(
+                email         = email,
+                password      = hashed_password,
+                date_of_birth = date_of_birth,
+                phone_number  = phone_number,
+            ).save()
+
+            return JsonResponse({'MESSAGE' : 'SUCCESS'}, status=200)
+        except KeyError:
+            return JsonResponse({'MESSAGE' : 'INVALID_KEY'}, status=400)
+        except ValueError:
+            return ValueErrorTypeChecking(data)
+
+
+class SignInView(View):
+    def post(self, request):
+        try:
+            data     = json.loads(request.body)
+            email    = data['email']
+            password = data['password']
+
+            user_validation = User.objects.filter(kakao_id__isnull=True).filter(email=email)
+
+            if not user_validation:
+                return JsonResponse({'MESSAGE' : 'INVALID_EMAIL_OR_PASSWORD'}, status=400)
+
+            signed_user =user_validation.get()
+            password_validation = bcrypt.checkpw(password.encode('utf-8'), signed_user.password.encode('utf-8'))
+
+            if not password_validation:
+                return JsonResponse({'MESSAGE' : 'INVALID_EMAIL_OR_PASSWORD'}, status=400)
+
+            access_token = jwt.encode({'id' : signed_user.id}, SECRET_KEY, algorithm=ALGORITHM)
+
+            return JsonResponse({'MESSAGE' : 'SUCCESS', 'ACCESS_TOKEN' : access_token}, status=200)
+        except ValueError:
+            return ValueErrorTypeChecking(data)
+        except KeyError:
+            return JsonResponse({'MESSAGE' : 'INVALID_KEY'}, status=400)
+
+
+
